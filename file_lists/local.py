bf235bf9284eccd227adb6065ea77fc2d4c3dc80
Caleb Groom <caleb@calebgroom.com>
2012-09-01 00:45:37 -0500

    Additional pep8 cleanup

--- file_lists/local.py
+++ file_lists/local.py
@@ -5,33 +5,43 @@ from datetime import datetime
 import re
 _log = Logging().log
 
+
 class DirectoryList(FileList):
-    def __init__(self,directory,follow=True,md5=True,exclude_patterns=None):
+    def __init__(self, directory, follow=True, md5=True,
+                 exclude_patterns=None):
         self.directory = directory
         self.follow = follow
         self.file_list = {}
         self.md5 = md5
         self.exclude_patterns = exclude_patterns or []
         self.updateList()
+
     def updateList(self):
-        for root,dirs,files in os.walk(self.directory,followlinks=self.follow):
+        for root, dirs, files in os.walk(self.directory,
+                                         followlinks=self.follow):
             for local_file in files:
                 local_file = os.path.normpath(local_file)
                 clean_root = os.path.normpath(root)
-                clean_root = os.path.relpath(clean_root,self.directory)
+                clean_root = os.path.relpath(clean_root, self.directory)
                 if clean_root == '.':
                     clean_root = ''
                 else:
                     clean_root += '/'
-                # if file path regex matches any of the exclude rules, then skip it.
+                # skip if file path regex matches any of the exclude rules
                 skip_file = False
                 for exclude_pattern in self.exclude_patterns:
-                    if len(re.findall(exclude_pattern, os.path.join(clean_root, local_file))) > 0:
-                        _log.info('file: "%s" skipped as it matches exclude rule: "%s"' % (os.path.join(clean_root,local_file), exclude_pattern))
+                    if len(re.findall(exclude_pattern,
+                                      os.path.join(clean_root,
+                                                   local_file))) > 0:
+                        _log.info('file: "%s" skipped as it matches exclude'
+                                  ' rule: "%s"' % (os.path.join(clean_root,
+                                                                local_file),
+                                                   exclude_pattern))
                         skip_file = True
+
                 if skip_file:
                     continue
-                
+
                 if self.md5:
                     try:
                         import hashlib
@@ -40,24 +50,37 @@ class DirectoryList(FileList):
                         import md5
                         self.hash = md5.new()
                     try:
-                        self.hash.update(open(str(root)+'/'+str(local_file),'rb').read())
+                        self.hash.update(open(str(root) + '/' +
+                                              str(local_file), 'rb').read())
                     except:
                         self.hash.update('')
                     try:
-                        self.file_list[clean_root+local_file] = {'name': clean_root+local_file,
-                                                             'hash': self.hash.hexdigest(),
-                                                             'size': os.path.getsize(str(root)+'/'+str(local_file)),
-                                                             'last_modified': datetime.fromtimestamp(os.path.getmtime(str(root)+'/'+str(local_file))),
-                                                             'local_path': self.directory+'/'+clean_root+local_file,
-                                                             'test': os.stat(str(root)+'/'+str(local_file)).st_mtime}
+                        self.file_list[clean_root + local_file] = {
+                            'name': clean_root + local_file,
+                            'hash': self.hash.hexdigest(),
+                            'size': os.path.getsize(str(root) + '/' +
+                                                    str(local_file)),
+                            'last_modified': datetime.fromtimestamp(
+                                os.path.getmtime(str(root) + '/' +
+                                                 str(local_file))),
+                            'local_path': (self.directory + '/' + clean_root +
+                                           local_file),
+                            'test': os.stat(str(root) + '/' +
+                                            str(local_file)).st_mtime}
                     except OSError:
-                        _log.warn('Could not access %s/%s skiping' % (str(root),str(local_file)))
+                        _log.warn('Could not access %s/%s skiping' % (
+                                  str(root), str(local_file)))
 
                 else:
                     try:
-                        self.file_list[clean_root+local_file] = {'name': clean_root+local_file,
-                                                             'size': os.path.getsize(str(root)+str(local_file)),
-                                                             'last_modified': datetime.fromtimestamp(os.path.getmtime(str(root)+str(local_file))),
-                                                             'local_path': self.directory+'/'+clean_root+local_file}
+                        self.file_list[clean_root + local_file] = {
+                            'name': clean_root + local_file,
+                            'size': os.path.getsize(str(root) +
+                                                    str(local_file)),
+                            'last_modified': datetime.fromtimestamp(
+                                os.path.getmtime(str(root) + str(local_file))),
+                            'local_path': (self.directory + '/' + clean_root +
+                                           local_file)}
                     except OSError:
-                        _log.warn('Could not access %s/%s skiping' % (str(root),str(local_file)))
+                        _log.warn('Could not access %s/%s skiping' % (
+                                  str(root), str(local_file)))
--- file_lists/swift.py
+++ file_lists/swift.py
@@ -5,17 +5,22 @@ from urllib import unquote
 from datetime import datetime
 _log = Logging().log
 
+
 class SwiftList(FileList):
-    def __init__(self,swift,container):
+    def __init__(self, swift, container):
         self.swift = swift
         self.container = container
         self.file_list = {}
         self.updateList()
         self.md5 = True
+
     def updateList(self):
         fl = self.swift.getFullFileList(self.container)
         for file in fl:
-            self.file_list[unquote(file)] = { 'name': fl[file]['name'],
-                                     'hash': fl[file]['hash'],
-                                     'size': fl[file]['bytes'],
-                                     'last_modified': datetime(*time.strptime(fl[file]['last_modified'][:19],"%Y-%m-%dT%H:%M:%S")[:6]) }
+            self.file_list[unquote(file)] = {
+                'name': fl[file]['name'],
+                'hash': fl[file]['hash'],
+                'size': fl[file]['bytes'],
+                'last_modified': datetime(*time.strptime(
+                                          fl[file]['last_modified'][:19],
+                                          "%Y-%m-%dT%H:%M:%S")[:6])}
